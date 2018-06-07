ed5d9d1ae7080cd6dbf562da6c4cc9890500dc15
kevinye <yfhhappy2012@gmail.com>
2018-06-07 12:59:40 +0800

    main tasks

---
+++ run.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from dindex import del_index
+from datetime import datetime, timedelta
+import time
+
+qi = del_index.QueryIndex(url='http://localhost:9200/_cat/indices?h=i,creation.date&format=json&pretty')
+res = qi.queryindex()
+
+list_delindex = []
+#get x days ago index
+def get_days_timestamp(x=0):
+	d = datetime.now()
+	dx = d - timedelta(days=x)
+	timest = int(time.mktime(dx.timetuple()))
+	return timest
+def delete_xdays_index():
+	t = get_days_timestamp(7)
+	for l in res():
+		ct = int(l['creation.date'][:10])
+		if ct < t:
+			if l['i'].startswith('.',0,1):
+				pass
+			else:
+				list_delindex.append(l['i'])
+		else:
+			pass	
+
+delete_xdays_index()
+#for i in list_delindex:
+#	print i
+
+for l in list_delindex:
+	delete_index = del_index.QueryIndex(url='http://localhost:9200/' + l)
+	response = delete_index.deleteindex()
+	print response,l
+
