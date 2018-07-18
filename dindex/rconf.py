08843dcf53622c943ad3af140f544571b58ff028
kevinye <yfhhappy2012@gmail.com>
2018-07-18 11:28:01 +0800

    fix dir

--- dindex/rconf.py
+++ dindex/rconf.py
@@ -1,14 +1,15 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+
 import ConfigParser
 import os
 
-path = os.getcwd()
-config_path = path + '/' + 'delastic.conf'
-print config_path
+current_path = os.path.abspath(__file__)
+father_path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".")
+config_file_path=os.path.join(os.path.abspath(os.path.dirname(current_path) + os.path.sep + ".."),'delastic.conf')
 def elastic_user():
 	auth = []
 	cfg = ConfigParser.ConfigParser()
-	cfg.readfp(open(config_path))
+	cfg.readfp(open(config_file_path))
 
 	auth = [cfg.get('elasticsearch','elastic_user'),cfg.get('elasticsearch','elastic_password')]
 	return tuple(auth)
